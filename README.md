8bb7bd794228c91c63b4a383b2f336cbe8e183fa
Hilmi Fauziyyah <126895059+HilmiFauziyyah@users.noreply.github.com>
2024-05-31 07:11:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fabric Network Builder
+
+Build fabric network config file(*.yml) for Fabric Java SDK or Fabric Gateway Java.
+
---
+++ fabric-network-builder-bbe/src/main/java/org/ecsoya/fabric/builder/BbeNetworkTest.java
@@ -0,0 +1,27 @@
+package org.ecsoya.fabric.builder;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.hyperledger.fabric.sdk.NetworkConfig;
+import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
+import org.hyperledger.fabric.sdk.exception.NetworkConfigurationException;
+
+public class BbeNetworkTest {
+
+	public static void main(String[] args) {
+		File file = new File("src/main/resources/network/connection-org1.yml");
+
+		try {
+			NetworkConfig network = NetworkConfig.fromYamlFile(file);
+			System.out.println(network);
+		} catch (InvalidArgumentException e) {
+			e.printStackTrace();
+		} catch (NetworkConfigurationException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
